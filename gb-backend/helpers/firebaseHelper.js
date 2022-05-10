56e5ec2784fefed7de255c3f082785a3343ccca2
Shubham-Kumar-2000 <shukhu10@gmail.com>
2022-05-11 02:07:38 +0530

    old flow'

--- gb-backend/helpers/firebaseHelper.js
+++ gb-backend/helpers/firebaseHelper.js
@@ -1,5 +1,5 @@
 const Admin = require('firebase-admin');
-const serviceAccount = require('../credentials/firebase.json');
+const serviceAccount = require('../credentials/firebase.js');
 
 Admin.initializeApp({
     credential: Admin.credential.cert(serviceAccount)
