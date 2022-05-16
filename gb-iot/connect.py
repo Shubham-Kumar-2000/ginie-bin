5942eb050c7c3cfc152ee61267b0f10a40dc879c
Shubham-Kumar-2000 <shukhu10@gmail.com>
2022-05-16 20:21:45 +0530

    bin sensors integrated

--- gb-iot/connect.py
+++ gb-iot/connect.py
@@ -1,35 +1,100 @@
 import socketio
 import signal
+import time
+import RPi.GPIO as GPIO
+import time
+import requests
+import json
+import sys
+from hx711 import HX711
+
+DHT_PIN = 24
+#Libraries
+
+GPIO.setwarnings(False)
+
+#GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+
+
+
+#set GPIO Pins
+GPIO_TRIGGER = 27
+GPIO_ECHO = 22
+
+#set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+
+hx= HX711(dout_pin=5,pd_sck_pin=6,gain=128)
+# hx.set_reading_format("MSB","MSB")
+# hx.set_reference_unit(1)
+
+hx.reset()
+# hx.tare()
+weight_ref=20380/50
+def getWeight():
+    weightdata = hx.get_raw_data()
+    weight = sum(weightdata)/len(weightdata)
+    return weight/weight_ref
+initial_weight = getWeight()
+
+def distance():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+
+    StartTime = time.time()
+    StopTime = time.time()
+
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+
+    return distance
 
 # standard Python
-sio = socketio.Client(logger=True, engineio_logger=True)
+sio = socketio.Client()
 
 BIN = {
     "open":False,
-    "height":100,
-    "weight":100,
+    "height":distance(),
+    "weight":initial_weight,
 }
 
 def completeBinTransaction(timeout):
     BIN['open']=False
-    change_height=110-BIN['height']
-    change_weight=110-BIN['weight']
+    change_height=distance()-BIN['height']
+    change_weight=getWeight()-BIN['weight']
     sio.emit('transaction', {'height': change_height, 'weight': change_weight,"timeout":timeout}, namespace="/bin")
 
 def handler(signum, stack):
     if(not BIN["open"]):
         return
     completeBinTransaction(timeout=True)
-    
 
-# signal.signal(signal.SIGALRM, handler)
+
+signal.signal(signal.SIGALRM, handler)
 
 
 @sio.on('open-bin',namespace="/bin")
 def on_open_bin(data):
     BIN['open']=True
-    BIN['height']=100
-    BIN['weight']=100
+    BIN['height']=distance()
+    BIN['weight']=getWeight()
     signal.alarm(60*5)
     print("Bin is open")
 
